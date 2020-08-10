fba1afd07339423464cae54c7b111d2abf0a9755
junicola <julia.nicola@hotmail.com>
2020-08-10 01:20:06 -0300

    estrutura do projeto

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text>Teste</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
